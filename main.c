b3c74c8ca233ba899f38eadd3a53169441d3ba76
Sarah Medeneche <meden.sarah@gmail.com>
2025-10-20 16:30:50 +0200

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 16:09:37 by smedenec          #+#    #+#             */
-/*   Updated: 2025/10/20 16:18:51 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/10/20 16:30:02 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ int	main(int argc, char **argv)
 {
 	int	answer;
 
+	argc = 1;
+	if(argv && argv[0])
+		argv[0][0] = '1';
 	answer = 0;
 	answer = sigusr1(1);
 	if (!answer)
